f044f8e3efc28cd2331b8cb81a7b8a3636766ea8
javaor <hxzhenu@gmail.com>
2021-05-31 08:04:19 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by xuan on 2021/5/31.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
